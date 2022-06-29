d295dfdb3e9ae79280d0850265fcfe90b69e867a
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-29 14:51:23 -0700

    fetch and env updated

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,9 @@
+
+
+
+export async function getPokemon(filter) {
+  const rawData = await fetch(`http://localhost:8888/.netlify/functions/pokemon?pokeQuery=${filter}`);
+  const data = await rawData.json();
+  
+  return data;
+}
