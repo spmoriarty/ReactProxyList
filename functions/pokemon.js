41324bcfd129861514424efae69b02114513d8b1
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-29 15:03:16 -0700

    building out pokemon fx for api

---
+++ functions/pokemon.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+// this line tells chrome to ignore CORS policy just in case
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch (`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${event.queryStringParamaters.pokeQuery}`);
+    const data = await respponse.json();
+    const json = JSON.stringify(data);
+    
+    return {
+      statusCode: 200;
+      headers, 
+      body: json };
+    }
+    catch error {
+      return {
+        statusCode: 500;
+        body: JSON.stringify({ error }),
+      }
+    }
+  }
